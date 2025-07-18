a2e097107d2107987773f69966a43b649a661e38
hdk-hanasis <hdkim@hanasis.com>
2025-07-18 09:51:45 +0900

    version: 3.0.0-alpha.6t

--- index.js
+++ index.js
@@ -585,40 +585,40 @@ Printer.prototype._getStyle = function (type) {
   switch (type.toUpperCase()) {
     case 'B':
       styled += _.TEXT_FORMAT.TXT_BOLD_ON
-      styled += _.TEXT_FORMAT.TXT_ITALIC_OFF
+      // styled += _.TEXT_FORMAT.TXT_ITALIC_OFF
       styled += _.TEXT_FORMAT.TXT_UNDERL_OFF
       break
-    case 'I':
-      styled += _.TEXT_FORMAT.TXT_BOLD_OFF
-      styled += _.TEXT_FORMAT.TXT_ITALIC_ON
-      styled += _.TEXT_FORMAT.TXT_UNDERL_OFF
-      break
+    // case 'I':
+    //   styled += _.TEXT_FORMAT.TXT_BOLD_OFF
+    //   styled += _.TEXT_FORMAT.TXT_ITALIC_ON
+    //   styled += _.TEXT_FORMAT.TXT_UNDERL_OFF
+    //   break
     case 'U':
       styled += _.TEXT_FORMAT.TXT_BOLD_OFF
-      styled += _.TEXT_FORMAT.TXT_ITALIC_OFF
+      // styled += _.TEXT_FORMAT.TXT_ITALIC_OFF
       styled += _.TEXT_FORMAT.TXT_UNDERL_ON
       break
     case 'U2':
       styled += _.TEXT_FORMAT.TXT_BOLD_OFF
-      styled += _.TEXT_FORMAT.TXT_ITALIC_OFF
+      // styled += _.TEXT_FORMAT.TXT_ITALIC_OFF
       styled += _.TEXT_FORMAT.TXT_UNDERL2_ON
       break
 
-    case 'BI':
-      styled += _.TEXT_FORMAT.TXT_BOLD_ON
-      styled += _.TEXT_FORMAT.TXT_ITALIC_ON
-      styled += _.TEXT_FORMAT.TXT_UNDERL_OFF
-      break
-    case 'BIU':
-      styled += _.TEXT_FORMAT.TXT_BOLD_ON
-      styled += _.TEXT_FORMAT.TXT_ITALIC_ON
-      styled += _.TEXT_FORMAT.TXT_UNDERL_ON
-      break
-    case 'BIU2':
-      styled += _.TEXT_FORMAT.TXT_BOLD_ON
-      styled += _.TEXT_FORMAT.TXT_ITALIC_ON
-      styled += _.TEXT_FORMAT.TXT_UNDERL2_ON
-      break
+    // case 'BI':
+    //   styled += _.TEXT_FORMAT.TXT_BOLD_ON
+    //   styled += _.TEXT_FORMAT.TXT_ITALIC_ON
+    //   styled += _.TEXT_FORMAT.TXT_UNDERL_OFF
+    //   break
+    // case 'BIU':
+    //   styled += _.TEXT_FORMAT.TXT_BOLD_ON
+    // styled += _.TEXT_FORMAT.TXT_ITALIC_ON
+    //   styled += _.TEXT_FORMAT.TXT_UNDERL_ON
+    //   break
+    // case 'BIU2':
+    //   styled += _.TEXT_FORMAT.TXT_BOLD_ON
+    //   styled += _.TEXT_FORMAT.TXT_ITALIC_ON
+    //   styled += _.TEXT_FORMAT.TXT_UNDERL2_ON
+    //   break
     case 'BU':
       styled += _.TEXT_FORMAT.TXT_BOLD_ON
       styled += _.TEXT_FORMAT.TXT_ITALIC_OFF
@@ -629,21 +629,21 @@ Printer.prototype._getStyle = function (type) {
       styled += _.TEXT_FORMAT.TXT_ITALIC_OFF
       styled += _.TEXT_FORMAT.TXT_UNDERL2_ON
       break
-    case 'IU':
-      styled += _.TEXT_FORMAT.TXT_BOLD_OFF
-      styled += _.TEXT_FORMAT.TXT_ITALIC_ON
-      styled += _.TEXT_FORMAT.TXT_UNDERL_ON
-      break
-    case 'IU2':
-      styled += _.TEXT_FORMAT.TXT_BOLD_OFF
-      styled += _.TEXT_FORMAT.TXT_ITALIC_ON
-      styled += _.TEXT_FORMAT.TXT_UNDERL2_ON
-      break
+    // case 'IU':
+    //   styled += _.TEXT_FORMAT.TXT_BOLD_OFF
+    //   styled += _.TEXT_FORMAT.TXT_ITALIC_ON
+    //   styled += _.TEXT_FORMAT.TXT_UNDERL_ON
+    //   break
+    // case 'IU2':
+    //   styled += _.TEXT_FORMAT.TXT_BOLD_OFF
+    //   styled += _.TEXT_FORMAT.TXT_ITALIC_ON
+    //   styled += _.TEXT_FORMAT.TXT_UNDERL2_ON
+    //   break
 
     case 'NORMAL':
     default:
       styled += _.TEXT_FORMAT.TXT_BOLD_OFF
-      styled += _.TEXT_FORMAT.TXT_ITALIC_OFF
+      // styled += _.TEXT_FORMAT.TXT_ITALIC_OFF
       styled += _.TEXT_FORMAT.TXT_UNDERL_OFF
       break
   }
